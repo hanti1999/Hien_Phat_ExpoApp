db9ba2a9781d86a0e8ff2a2fb3e665a117e01f4c
Hoàng Anh <nth.anh020209@gmail.com>
2024-03-18 22:45:19 +0700

    add Tailwind, create Navigation

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native & Expo!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <>
+      <StackNavigator />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
