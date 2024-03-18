7194282b050783832af9e3a846a628ad18bfef52
Hoàng Anh <nth.anh020209@gmail.com>
2024-03-18 11:58:45 +0700

    fix bugs

--- api/index.js
+++ api/index.js
@@ -3,29 +3,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 
 const app = express();
 const port = 8000;
 const cors = require('cors');
 app.use(cors());
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const jwt = require('jsonwebtoken');
 
-mongoose
-  .connect('mongodb+srv://hoanganh:0comatkhaU@cluster0.nsblzgf.mongodb.net/', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDb');
-  })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB', err);
-  });
+// database connection
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB database connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.listen(port, () => {
-  console.log('Server is running on port: ', port);
+  connect();
+  console.log('Server is running on port:', port);
 });
