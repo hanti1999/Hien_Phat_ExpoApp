9cd73fcb14754b394bd54e1cdd91f170a466582e
Hoàng Anh <nth.anh020209@gmail.com>
2024-04-15 23:06:10 +0700

    fix ux/ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
   Pressable,
   TextInput,
   Alert,
+  Dimensions,
 } from 'react-native';
 import { Ionicons, Entypo } from '@expo/vector-icons';
 import { SliderBox } from 'react-native-image-slider-box';
@@ -162,11 +163,108 @@ const gasStove = [
   },
 ];
 
+const electriceStore = [
+  {
+    id: 0,
+    title: 'Bếp điện đơn Sakura SE3150B',
+    oldPrice: 1250000,
+    price: 999000,
+    image: require('../assets/products/sakura-se3150b.jpg'),
+    carouselImages: [
+      require('../assets/products/sakura-se3150b.jpg'),
+      require('../assets/products/sakura-se3150b.jpg'),
+      require('../assets/products/sakura-se3150b.jpg'),
+      require('../assets/products/sakura-se3150b.jpg'),
+    ],
+  },
+  {
+    id: 1,
+    title: 'Bếp từ đơn Kaff KF330i',
+    oldPrice: 3080000,
+    price: 2310000,
+    image: require('../assets/products/kaff-kf330i.jpg'),
+    carouselImages: [
+      require('../assets/products/kaff-kf330i.jpg'),
+      require('../assets/products/kaff-kf330i.jpg'),
+      require('../assets/products/kaff-kf330i.jpg'),
+      require('../assets/products/kaff-kf330i.jpg'),
+    ],
+  },
+  {
+    id: 2,
+    title: 'Bếp từ đôi Chiffs CH112S',
+    oldPrice: 8500000,
+    price: 5950000,
+    image: require('../assets/products/chiffs-ch112s.jpg'),
+    carouselImages: [
+      require('../assets/products/chiffs-ch112s.jpg'),
+      require('../assets/products/chiffs-ch112s.jpg'),
+      require('../assets/products/chiffs-ch112s.jpg'),
+      require('../assets/products/chiffs-ch112s.jpg'),
+    ],
+  },
+  {
+    id: 3,
+    title: 'Bếp điện từ Kaff KF-FL866GIH',
+    oldPrice: 14800000,
+    price: 11100000,
+    image: require('../assets/products/kaff-kffl866gih.jpg'),
+    carouselImages: [
+      require('../assets/products/kaff-kffl866gih.jpg'),
+      require('../assets/products/kaff-kffl866gih.jpg'),
+      require('../assets/products/kaff-kffl866gih.jpg'),
+      require('../assets/products/kaff-kffl866gih.jpg'),
+    ],
+  },
+];
+
+const waterPuri = [
+  {
+    id: 0,
+    title: 'Máy lọc nước để gầm Mutosi MP370U',
+    oldPrice: 5925000,
+    price: 2962500,
+    image: require('../assets/products/mutosi-mp370u.jpg'),
+    carouselImages: [
+      require('../assets/products/mutosi-mp370u.jpg'),
+      require('../assets/products/mutosi-mp370u.jpg'),
+      require('../assets/products/mutosi-mp370u.jpg'),
+      require('../assets/products/mutosi-mp370u.jpg'),
+    ],
+  },
+  {
+    id: 1,
+    title: 'Máy lọc nước nóng nguội Mutosi MP582H',
+    oldPrice: 7690000,
+    price: 3845000,
+    image: require('../assets/products/mutosi-mp582h.jpg'),
+    carouselImages: [
+      require('../assets/products/mutosi-mp582h.jpg'),
+      require('../assets/products/mutosi-mp582h.jpg'),
+      require('../assets/products/mutosi-mp582h.jpg'),
+      require('../assets/products/mutosi-mp582h.jpg'),
+    ],
+  },
+  {
+    id: 2,
+    title: 'Máy lọc nước DaikioSan DSW32009H2',
+    oldPrice: 5300000,
+    price: 3975000,
+    image: require('../assets/products/daikiosan-dsw-32009h2.jpg'),
+    carouselImages: [
+      require('../assets/products/daikiosan-dsw-32009h2.jpg'),
+      require('../assets/products/daikiosan-dsw-32009h2.jpg'),
+      require('../assets/products/daikiosan-dsw-32009h2.jpg'),
+      require('../assets/products/daikiosan-dsw-32009h2.jpg'),
+    ],
+  },
+];
+
 const HomeScreen = () => {
   return (
     <SafeAreaView
-      className=' flex-1'
-      style={{ paddingTop: Platform.OS == 'android' ? 40 : 0 }}
+      className=' flex-1 bg-primary-pink'
+      style={{ paddingTop: Platform.OS == 'android' ? 20 : 0 }}
     >
       <ScrollView stickyHeaderIndices={[0]} className='bg-gray-100'>
         <SearchBar />
@@ -186,7 +284,10 @@ const HomeScreen = () => {
           <Text className='font-bold text-2xl p-2.5 text-red-500'>
             Khuyến Mãi Hot
           </Text>
-          <Pressable className='flex-row items-center'>
+          <Pressable
+            onPress={() => Alert.alert('Thông báo', 'Clicked')}
+            className='flex-row items-center'
+          >
             <Text className='text-blue-400 font-medium'>
               Xem thêm khuyến mãi
             </Text>
@@ -198,40 +299,57 @@ const HomeScreen = () => {
           {sale.map((item, index) => (
             <ProductCard item={item} key={index} />
           ))}
-          <Pressable className='h-[320px] w-[180px]'>
-            <View className='flex-row w-full h-full justify-center items-center border border-gray-200 rounded-md'>
-              <Text className='text-blue-400'>Xem Thêm</Text>
-              <Entypo name='chevron-right' size={24} color='#60a5fa' />
-            </View>
-          </Pressable>
+          <SeeMoreCard />
         </ScrollView>
 
         <View className='border-t-2 border-primary-pink mt-5 relative bg-white'>
-          <Pressable>
-            <View
-              className='absolute -top-5 left-1/2 z-1 w-[200px]'
-              style={{ transform: [{ translateX: -100 }] }}
-            >
-              <View className='absolute border-x-[12px] border-b-[20px] border-x-transparent border-b-pink-500 -right-3 top-0' />
-              <View className='absolute border-x-[12px] border-b-[20px] border-x-transparent border-b-pink-500 -left-3 top-0' />
-              <View className='bg-primary-pink relative flex w-full items-center justify-center rounded-b-lg py-1'>
-                <Text className='font-semibold text-xl text-white uppercase'>
-                  Bếp Gas
-                </Text>
-              </View>
-            </View>
-          </Pressable>
+          <Badge text={'Bếp gas'} />
 
           <ScrollView horizontal className='px-2 mt-10'>
             {gasStove.map((item, index) => (
               <ProductCard item={item} key={index} />
             ))}
-            <Pressable className='h-[320px] w-[180px]'>
-              <View className='flex-row w-full h-full justify-center items-center border border-gray-200 rounded-md'>
-                <Text className='text-blue-400'>Xem Thêm</Text>
-                <Entypo name='chevron-right' size={24} color='#60a5fa' />
-              </View>
-            </Pressable>
+            <SeeMoreCard />
+          </ScrollView>
+        </View>
+
+        <View>
+          <Pressable onPress={() => Alert.alert('Thông báo', 'Clicked')}>
+            <Image
+              className='w-full h-[60px]'
+              source={require('../assets/sale8-3.jpg')}
+            />
+          </Pressable>
+        </View>
+
+        <View className='border-t-2 border-primary-pink mt-5 relative bg-white'>
+          <Badge text={'Bếp điện'} />
+
+          <ScrollView horizontal className='px-2 mt-10'>
+            {electriceStore.map((item, index) => (
+              <ProductCard item={item} key={index} />
+            ))}
+            <SeeMoreCard />
+          </ScrollView>
+        </View>
+
+        <View>
+          <Pressable onPress={() => Alert.alert('Thông báo', 'Clicked')}>
+            <Image
+              className='w-full h-[140px]'
+              source={require('../assets/DonTetSaleHet_MayLocNuoc.png')}
+            />
+          </Pressable>
+        </View>
+
+        <View className='border-t-2 border-primary-pink mt-5 relative bg-white'>
+          <Badge text={'Máy lọc nước'} />
+
+          <ScrollView horizontal className='px-2 mt-10'>
+            {waterPuri.map((item, index) => (
+              <ProductCard item={item} key={index} />
+            ))}
+            <SeeMoreCard />
           </ScrollView>
         </View>
       </ScrollView>
@@ -241,7 +359,10 @@ const HomeScreen = () => {
 
 const ProductCard = ({ item }) => {
   return (
-    <Pressable className='w-[180px] max-h-[320px] pr-2 mb-2'>
+    <Pressable
+      onPress={() => Alert.alert('Thông báo', 'Clicked')}
+      className='w-[180px] max-h-[320px] pr-2 mb-2'
+    >
       <View className='border h-full flex justify-between border-gray-200 rounded-md overflow-hidden'>
         <View>
           <Image
@@ -250,7 +371,7 @@ const ProductCard = ({ item }) => {
             source={item?.image}
           />
           <View className='mx-2'>
-            <Text className='max-w-full'>{item?.title}</Text>
+            <Text numberOfLines={2}>{item?.title}</Text>
             <Text className='line-through'>
               {item?.oldPrice.toLocaleString()}
             </Text>
@@ -261,9 +382,47 @@ const ProductCard = ({ item }) => {
         </View>
 
         <View className='flex justify-items-end'>
-          <Pressable className=' bg-primary-pink'>
+          <Pressable
+            onPress={() => Alert.alert('Thông báo', 'Clicked')}
+            className=' bg-primary-pink'
+          >
             <Text className='text-center py-2 text-white text-lg'>MUA</Text>
           </Pressable>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+const SeeMoreCard = () => {
+  return (
+    <Pressable
+      onPress={() => Alert.alert('Thông báo', 'Clicked')}
+      className='h-[320px] w-[180px]'
+    >
+      <View className='w-full h-full justify-center items-center'>
+        <Text className='text-blue-400'>Xem Thêm</Text>
+        <View className=' bg-gray-100 w-10 h-10 rounded-full justify-center mt-2 items-center'>
+          <Entypo name='chevron-right' size={24} color='#60a5fa' />
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+const Badge = ({ text }) => {
+  return (
+    <Pressable onPress={() => Alert.alert('Thông báo', 'Clicked')}>
+      <View
+        className='absolute -top-5 left-1/2 z-1 w-[200px]'
+        style={{ transform: [{ translateX: -100 }] }}
+      >
+        <View className='absolute border-x-[12px] border-b-[20px] border-x-transparent border-b-pink-500 -right-3 top-0' />
+        <View className='absolute border-x-[12px] border-b-[20px] border-x-transparent border-b-pink-500 -left-3 top-0' />
+        <View className='bg-primary-pink relative flex w-full items-center justify-center rounded-b-lg py-1'>
+          <Text className='font-semibold text-xl text-white uppercase'>
+            {text}
+          </Text>
         </View>
       </View>
     </Pressable>
@@ -273,7 +432,10 @@ const ProductCard = ({ item }) => {
 const SearchBar = () => {
   return (
     <View className='bg-primary-pink'>
-      <Pressable className='flex-row items-center bg-white h-10 rounded-md mx-4 my-2.5'>
+      <Pressable
+        onPress={() => Alert.alert('Thông báo', 'Clicked')}
+        className='flex-row items-center bg-white h-10 rounded-md mx-4 my-2.5'
+      >
         <Ionicons
           style={{ paddingHorizontal: 10 }}
           name='search'
@@ -290,7 +452,11 @@ const Category = () => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {list.map((item, index) => (
-        <Pressable key={index} className='m-1'>
+        <Pressable
+          onPress={() => Alert.alert('Thông báo', 'Clicked')}
+          key={index}
+          className='m-1'
+        >
           <Image
             resizeMode='contain'
             className='w-20 h-20'
