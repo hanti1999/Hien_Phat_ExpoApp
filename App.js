9c41a01bf7fe5dd3cf26b187348f37984510d63d
Hoàng Anh <nth.anh020209@gmail.com>
2024-05-01 21:57:56 +0700

    add redux toolkit

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux';
 import StackNavigator from './navigation/StackNavigator';
+import { store } from './store';
 
 export default function App() {
   return (
     <>
-      <StackNavigator />
+      <Provider store={store}>
+        <StackNavigator />
+      </Provider>
     </>
   );
 }
